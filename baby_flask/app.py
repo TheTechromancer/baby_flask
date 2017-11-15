c816b68ae479c5ef01ba76acf40fa592fe5c1355
TheTechromancer <thetechromancer@protomail.com>
2017-11-15 15:12:15 -0600

    Artified

--- baby_flask/app.py
+++ baby_flask/app.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 # import Flask class (an instance of this class will be our WSGI application)
 # render_template generates HTML dynamically given a template HTML file
 from flask import Flask, url_for, render_template
+from random import randint
 
 # __name__ is used because depending on if it's started as an application or
 # imported as a module, the name will be different.  This is needed so that
@@ -35,15 +38,62 @@ app_name = "Baby Flask"
 @app.route('/')
 def home():
 
-	return render_template('home.html')
+	themes = ["sw", "asoiaf"]
 
-	# returns the message we want to display in the user's browser
-	return 'Hello there.\n<a href={}>{}</a>'.format(url_for('admin'), 'admin')
+	arts = {
+		"sw": [
+			"https://orig00.deviantart.net/6853/f/2017/275/a/c/star_wars___siege_of_mandalore_by_thetechromancer-dbp1v6o.png",
+			"https://orig00.deviantart.net/3b32/f/2017/203/c/1/star_wars___lost_child__ahsoka_tano__by_thetechromancer-dbh9w2n.png",
+			"https://orig00.deviantart.net/ce61/f/2015/301/a/b/star_wars___whispers_of_the_past__ahsoka_tano__by_thetechromancer-d7z38st.png",
+			"https://orig00.deviantart.net/4a16/f/2015/310/2/f/star_wars___darth_revan_vs__bastila_shan_by_thetechromancer-d9fpt1m.png",
+			"https://orig00.deviantart.net/6b64/f/2016/316/9/7/star_wars___peacekeeper__obi_wan_kenobi__by_thetechromancer-dao7hka.png"
+		],
+		"asoiaf": [
+			"https://orig00.deviantart.net/e766/f/2017/319/1/6/trident_by_thetechromancer-dbtvdg9.jpg",
+			"https://orig00.deviantart.net/70e0/f/2017/319/f/b/targaryens_by_thetechromancer-dbtvdfw.jpg",
+			"https://orig00.deviantart.net/1823/f/2017/319/4/2/stoneheart_by_thetechromancer-dbtvjrk.png",
+			"https://orig00.deviantart.net/a389/f/2017/319/f/1/rhaegar_by_thetechromancer-dbtvdfh.jpg"
+		]
+	}
+
+	backgrounds = {
+		"sw": [ "https://steamuserimages-a.akamaihd.net/ugc/251464760146726875/760F72CF39FD7D13B418EC008A92F7B23150BC4A/" ],
+		"asoiaf": [ "https://i.pinimg.com/originals/a2/dd/5a/a2dd5ae7059d1a47fbf7ee2bbb3fc0fe.jpg" ]
+	}
+
+	try:
+		theme = themes[app.config['THEME']]
+	except:
+		theme = pick_random(themes)
+
+	art = pick_random(arts[theme])
+	background = pick_random(backgrounds[theme])
+
+	return render_template('home.html', rand_art=art, background=background)
+
 
 @app.route('/admin/')
 def admin():
 
 	return render_template('admin.html')
+
+
+def pick_random(v, key=None):
+	'''
+	takes dictionary or iterable
+	if dictionary, picks a random key and then picks random entry from associated value
+	if list, picks random entry
+	'''
+	if type(v) == dict:
+		try:
+			v = v[key]
+		except:
+			values = list(v.values())
+			v = values[randint(0,len(values)-1)]
+
+	r = v[randint(0,len(v)-1)]
+	print(r)
+	return r
 
 if __name__ == '__main__':
 	app.run()
